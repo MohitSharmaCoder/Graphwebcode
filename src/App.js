148a3b35f1188e5226537003d2fe676341b05f7c
MohitSharmaCoder <mohitsharma20036396@gmail.com>
2022-06-22 18:29:34 -0100

    second

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react'
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './component/Navbar'
 import SectionOne from './component/SectionOne';
 import SideNav from './component/SideNav';
 const App = () => {
   return (
     <div>
-      <BrowserRouter>
+      {/* <BrowserRouter> */}
 			<div className="section-one">
 			</div>
 			<div className="app-nav"><Navbar/></div>
 			<div className="section-tt">
 				<div className='appa'><SideNav/></div>
 				<div className="appb">
-				<Routes>
-					<Route path="/" element={<SectionOne/>} />
-				</Routes>	
+				<SectionOne/>
+				
 				</div>
 			</div>
-      </BrowserRouter>
+      {/* </BrowserRouter> */}
     </div>
   )
 }
